93cc41b81cb265d1669a8c00e22e03197ed7d0b6
cobracommunity0-creator <cobracommunity0@gmail.com>
2026-09-07 00:57:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -429,7 +429,7 @@ app.post('/api/admin/force-close-shift', async (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`🚀 شغال على البورت ${PORT} - حواسب كافيه ❤️`));
